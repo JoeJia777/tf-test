ad006c6c836c5b877e5dca1587aa58dc9bf0b467
JoeJia777 <joejia777@outlook.com>
2023-08-13 16:33:54 +1000

    first commit to tftest branch

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+
+}
+
+variable "secret_key" {
+
+}
+
+variable "aws_region" {
+  default = "ap-southeast-2"
+}
+
+variable "root_domain" {
+  default = "tf.bydyangwang.com"
+}
+
+variable "hosted_zone_id" {
+  default = "Z0856109DI1T5TF3YXYV"
+}
